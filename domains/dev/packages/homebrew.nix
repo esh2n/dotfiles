7ebf607e1f14a8a6ba851ed0b4eeeb1dbce457e6
esh2n <esh2n.bz+github@gmail.com>
2026-02-21 23:33:36 +0900

    feat: fix sketchybar_lua

--- domains/dev/packages/homebrew.nix
+++ domains/dev/packages/homebrew.nix
@@ -8,6 +8,7 @@
   ];
 
   homebrew.casks = [
+    "android-studio"
     "warp"
   ];
 }
--- domains/workspace/config/sketchybar/helpers/init.lua
+++ domains/workspace/config/sketchybar/helpers/init.lua
@@ -1,4 +1,6 @@
 -- Add the sketchybar module to the package cpath
 package.cpath = package.cpath .. ";/Users/" .. os.getenv("USER") .. "/.local/share/sketchybar_lua/?.so"
 
-os.execute("(cd helpers && make)")
+-- Use absolute path to avoid issues when launched from launchd
+local config_dir = os.getenv("HOME") .. "/.config/sketchybar"
+os.execute("(cd " .. config_dir .. "/helpers && make)")
